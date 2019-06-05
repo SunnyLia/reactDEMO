43b5e6a4f1654c32266b7e7b222aef24424027c0
SunnyLia <1326155622@qq.com>
2019-06-05 14:28:07 +0800

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -218,3 +218,48 @@ class ErrorBoundary extends React.Component {
         return this.props.children;
     }
 }
+
+/*refs转发*/
+// Ref转发是一项将ref自动地通过组件传递到其一子组件的技巧
+// 1、转发refs到DOM组件
+const FancyButton = React.forwardRef((props, ref) => (
+    <button ref={ref} className="FancyButton">
+        {props.children}
+    </button>
+));
+
+// 你可以直接获取 DOM button 的 ref：
+const ref = React.createRef();
+<FancyButton ref={ref}>Click me!</FancyButton>;
+/*以下是对上述示例发生情况的逐步解释：
+
+    1、我们通过调用 React.createRef 创建了一个 React ref 并将其赋值给 ref 变量。
+    2、我们通过指定 ref 为 JSX 属性，将其向下传递给 <FancyButton ref={ref}>。
+    3、React 传递 ref 给 fowardRef 内函数 (props, ref) => ...，作为其第二个参数。
+    4、我们向下转发该 ref 参数到 <button ref={ref}>，将其指定为 JSX 属性。
+    5、当 ref 挂载完成，ref.current 将指向 <button> DOM 节点。*/
+
+// 我们可以使用 React.forwardRef API 明确地将 refs 转发到内部的 FancyButton 组件。
+// React.forwardRef 接受一个渲染函数，其接收 props 和 ref 参数并返回一个 React 节点。
+function logProps(Component) {
+    class LogProps extends React.Component {
+        componentDidUpdate(prevProps) {
+            console.log('old props:', prevProps);
+            console.log('new props:', this.props);
+        }
+
+        render() {
+            const { forwardedRef, ...rest } = this.props;
+
+            // 将自定义的 prop 属性 “forwardedRef” 定义为 ref
+            return <Component ref={forwardedRef} {...rest} />;
+        }
+    }
+
+    // 注意 React.forwardRef 回调的第二个参数 “ref”。
+    // 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
+    // 然后它就可以被挂载到被 LogPros 包裹的子组件上。
+    return React.forwardRef((props, ref) => {
+        return <LogProps {...props} forwardedRef={ref} />;
+    });
+}
