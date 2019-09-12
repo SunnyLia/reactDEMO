5820a901ee51c1a4e9904b01fee8212adcd06384
SunnyLia <1326155622@qq.com>
2019-09-12 12:30:44 +0800

    Update demo1.js

--- src/demo1.js
+++ src/demo1.js
@@ -1,9 +1,12 @@
 /* JSX简介 */
 // 什么是jsx?
 // jsx是JavaScript得语法扩展，可以很好得描述ui应该呈现出它应有交互得本质形式，还可以使react显示更多有用得错误喝警告消息。
-// 1、在jsx中嵌入表达式
+// 1、可以在jsx中用大括号{}包裹嵌入表达式
+// 2、在编译之后，JSX表达式会被转为普通js函数调用，并且对其取值后得到JS对象
+// 3、同一属性不能同时使用引号跟大括号这两种符号，在属性中嵌入js表达式时，不能在大括号外面加上引号。
+//    同时，react Dom使用小驼峰命名来定义属性的名称，而不使用HTML属性名称的命名约定。
 // 2、使用jsx指定子元素
-// 3、jsx防止注入攻击,会默认对输入内容进行转义。
+// 3、jsx可防止注入攻击,在渲染时会默认对输入内容进行转义。
 // 4、Babel会把jsx转译成名为React.createElement()函数调用。
 const name = "JJ Liu";
 const ele = <h1 className="greeting">hello,{name}</h1>
@@ -27,18 +30,20 @@ const ele2 = {
 
 /*元素渲染 */
 ReactDOM.render(ele, document.getElementById("root"))
+reactDOM.render()可以将一个元素渲染为DOM
 // react元素是不可变对象，一旦被创建，就无法更改它的子元素或属性。
-// 所以，更新Ui唯一的方式是创建一个全新的元素，并将其传入ReactDOM.render()中
-// 但，React Dom会将元素和它的子元素与他们之前的状态进行比较，并只会只会更新实际改变了的内容来使DOM达到预期的状态。
+// 所以，更新Ui(已渲染的元素)唯一的方式是创建一个全新的元素，并将其传入ReactDOM.render()中
+// 但，React Dom会将元素和它的子元素与他们之前的状态进行比较，并只会只会更新实际改变了的内容（它需要更新的部分）来使DOM达到预期的状态。
 
 
 
 /*组件&props */
 // 函数组件
+// 接收唯一带有数据的props对象，并返回一个React元素。
 function Welcome(props) {
   return <h1>hello,{props.name}</h1>
 }
-// class组件
+// ES6 class组件
 class Welcom extends React.Component {
   render() {
     return <h1>hello,{this.props.name}</h1>
@@ -51,6 +56,7 @@ function Welcom(props) {
 }
 const ele = <Welcome name="Sara" />;
 ReactDOM.render(ele, document.getElementById("root"))
+
 // 组合组件
 // 组件可以在其输出中引用其他组件。
 function Welcome(props) {
@@ -71,14 +77,11 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
+//Props是只读的，组件不能修改自身的props
+//纯函数：函数不会尝试更改入参，且多次调用下相同的入参始终返回相同的结果
 
 /* state&生命周期 */
-
-// 向class组件中添加局部的state
-// 1、把render()方法中的this.props.date替换成this,state.date
-// 2、添加一个class构造函数，然后再改函数中为this.stae赋初值
-// 3、移除<Clock />元素中的date属性
+//state与props类似，但是state是私有的，并且完全受控于当前组件
 class Clock extends React.Component {
   // 将 props 传递到父类的构造函数中
   constructor(props) {
@@ -101,49 +104,35 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// 将生命周期方法添加到class中
-// componentDidMount()方法会在组件已经被渲染到DOM中后运行，所以，最好再这里设置计时器
-// componentWillUnmount()方法会在组件被删除的时候运行，所以在这里清除计时器
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
+//使用state的注意事项：
+//1）不要直接修改state，因为不会重新渲染组件，而是通过this.setState() 来更新组件state
+//2）state的更新可能是异步的
+    // 处于性能考虑，React可能会把多个setState（）调用合并成一个调用。
+    // 因为this.props和this.state可能会异步更新，所以不能依赖他们的值来更新下一个状态。
+  //解决方法
+    //让setState（）接收一个函数而不是一个对象。这个函数用上一个state作为第一个参数，将此次更新被应用时的props作为第二个参数：
+    // Wrong
     this.setState({
-      date: new Date()
+      counter: this.state.counter + this.props.increment,
     });
-    // this.setState((state) => ({
-    //   date: !state.date  //用state中的值修改state
-    // }));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
-);
+    // Correct
+    this.setState((state, props) => ({
+      counter: state.counter + props.increment
+    }));
+    // Correct
+    this.setState(function(state, props) {
+      return {
+        counter: state.counter + props.increment
+      };
+    });
+//3)state的更新会被合并
+  //因为当调用setState（）的时候，React会把提供的对象合并到当前的state
+  //可以分别调用setState（）来单独更新state。
+//4）单向数据流（数据是向下流的）
+  //组件可以选择把它的state作为props向下传递到他的子组件中
+  //同时，子组件本身无法知道接收到的数据是来自父组件的state还是props，还是手动输入的。
+  //单向数据流：
+    //任何的 state 总是所属于特定的组件，而且从该 state 派生的任何数据或 UI 只能影响树中“低于”它们的组件。
 
 /*事件处理 */
 // 阻止默认行为 e.preventDefault();
