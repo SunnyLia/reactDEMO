87dd3fb6098fb0f1368af99be0ff09802ed54556
SunnyLia <1326155622@qq.com>
2020-01-10 18:34:14 +0800

    Update demo3.js

--- src/demo3.js
+++ src/demo3.js
@@ -109,9 +109,32 @@
     //缓冲区长度（返回Buffer对象所占据的内存长度）
         buf.length
     
-    
+/*Node Stream*/    
+    Stream（流）是一个抽象接口，node中很多对象实现了这个接口，例如，对http 服务器发起请求的request 对象就是一个 Stream，还有stdout（标准输出）。
+    node中Stream有四种流类型：
+        1）Readable-可读操作
+        2）Writabel-可写操作
+        3）Duplex-可读可写操作
+        4）Transform-操作被写入数据，然后读出结果
+    所有的Stream对象都是EventEmitter的实例，常用的事件有：
+        1）data-当有数据可读时触发
+        2）end-没有更多的数据可读时触发
+        3）error-在接收和写入过程中发生错误时触发
+        4）finish-所有数据已被写入到底层系统时触发
+    //从流中读取数据
+        var fs = require("fs");
+        var data = 'hello ';
+        // 创建可读流
+        var readerStream = fs.createReadStream('input.txt'); //input.txt里面写了 world~
+        // 设置编码为 utf8。
+        readerStream.setEncoding('UTF8');
+        // 处理流事件 --> data, end, and error
+        readerStream.on('data', function(chunk) {
+           console.log(data + chunk); //打印 hello world~
+        });
 
-
+    //写入流
+        
 
 
 
