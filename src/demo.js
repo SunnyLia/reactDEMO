d4c1f41a21e6138596408424357227d51e6e54bb
SunnyLia <1326155622@qq.com>
2020-01-10 10:29:39 +0800

    Update and rename demo.js to demo3.js

--- src/demo.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react';
-import './index.less'
-export default class Home extends Component {
-    render () {
-        return (
-            <div>Home</div>
-        )
-    }
-}
---
+++ src/demo3.js
@@ -0,0 +1,3 @@
+/*node介绍*/
+    简单的说，node.js就是运行在服务端的JavaScript。是一个事件驱动I/O服务端JavaScript环境，基于Google的v8引擎，使执行JavaScript的速度更快，性能更好。
+    
